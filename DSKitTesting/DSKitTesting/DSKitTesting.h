4275c78bbb8bffa16479b50b9e06c0e00768df06
Dimitar Stefanovski <dimebt_2005@hotmail.com>
2019-04-12 11:22:37 +0200

    First commit

---
+++ DSKitTesting/DSKitTesting/DSKitTesting.h
@@ -0,0 +1,19 @@
+//
+//  DSKitTesting.h
+//  DSKitTesting
+//
+//  Created by Dimitar Stefanovski on 4/12/19.
+//  Copyright © 2019 Dimitar Stefanovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSKitTesting.
+FOUNDATION_EXPORT double DSKitTestingVersionNumber;
+
+//! Project version string for DSKitTesting.
+FOUNDATION_EXPORT const unsigned char DSKitTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSKitTesting/PublicHeader.h>
+
+
---
+++ DSKitTesting/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  DSKitTesting
+//
+//  Created by Dimitar Stefanovski on 4/12/19.
+//  Copyright © 2019 Dimitar Stefanovski. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() { }
+    
+    public static func doSomething() -> String {
+        return "This is awaesome!!"
+    }
+}
